b590594c43feb1d38f5b0685f7545886a58dab2f
MIRUNA POP <mirunapop2002@gmail.com>
2021-09-07 13:47:00 +0200

    Updates

---
+++ precache-manifest.2cebfd26338d4f80ef9704645642842d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a55e806acfcc4c59737d7e0d983b50e",
+    "url": "/restaurantReact/index.html"
+  },
+  {
+    "revision": "7185b86788ce5c0a7944",
+    "url": "/restaurantReact/static/css/2.b24af79a.chunk.css"
+  },
+  {
+    "revision": "bdfd2934f78912901879",
+    "url": "/restaurantReact/static/css/main.a5592d7c.chunk.css"
+  },
+  {
+    "revision": "7185b86788ce5c0a7944",
+    "url": "/restaurantReact/static/js/2.717e739e.chunk.js"
+  },
+  {
+    "revision": "4adfa064f4129dc9243483f188ee5829",
+    "url": "/restaurantReact/static/js/2.717e739e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdfd2934f78912901879",
+    "url": "/restaurantReact/static/js/main.f4bd8972.chunk.js"
+  },
+  {
+    "revision": "799c2841759af0a58827",
+    "url": "/restaurantReact/static/js/runtime-main.bd5bf57f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurantReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurantReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurantReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurantReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurantReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
